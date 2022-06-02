d71c808d165f6a982b3041017680ef255bf821e2
Garbarkozaurus <marcin.golski@student.put.poznan.pl>
2022-06-02 18:25:58 +0200

    Move histograms to a cell in a grid
    Also restructured the site structure - I don't think that more than 2
    pages are reasonable. Probably should get rid of the sidebar, and just
    use a navbar at the top for switching between "help" and the dashboard
    proper.

--- ui.R
+++ ui.R
@@ -12,8 +12,7 @@ dashboardPage(
         tags$a(tags$img(src="PP_logotyp_ANG_RGB.png", height="100%", width="100%")),
         sidebarMenu(
             menuItem("Home", tabName = "home_tab", icon=icon("home")),
-            menuItem("Histograms", tabName = "histograms_tab"),
-            menuItem("Handling missing values", tabName = "missing_values_tab")
+            menuItem("Dashboard", tabName = "dashboard_tab")
         )
      ),
     
@@ -25,33 +24,48 @@ dashboardPage(
                 p("Add some generic info about the dataset"),
                 p("And maybe a very basic visualisation?")
             ),
-            tabItem(tabName = "histograms_tab",
-            verticalLayout(
-                # hack to have the slider centered
-                column(width=12,
-                       sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
-                        align="center"),
+            tabItem(tabName = "dashboard_tab",
+            fluidRow(
+                column(width=4,
                 
-                fluidRow(column(width=4, plotOutput("ph_hist")), 
-                         column(width=4, plotOutput("hardness_hist")), 
-                         column(width=4, plotOutput("solids_hist"))),
-                
-                fluidRow(column(width=4, plotOutput("chloramines_hist")),
-                         column(width=4, plotOutput("sulfate_hist")),
-                         column(width=4, plotOutput("conductivity_hist"))),
-                
-                fluidRow(column(width=4, plotOutput("organic_carbon_hist")),
-                         column(width=4, plotOutput("trihalomethanes_hist")),
-                         column(width=4, plotOutput("turbidity_hist")))
-            )
+                   verticalLayout(
+                       # hack to have the slider centered
+                       column(width=12,
+                              sliderInput("bins",
+                                          "Number of bins:",
+                                          min = 1,
+                                          max = 50,
+                                          value = 30),
+                                          align="center"),
+                    navlistPanel(
+                        tabPanel("pH", plotOutput("ph_hist")),
+                        tabPanel("Hardness", plotOutput("hardness_hist")),
+                        tabPanel("Solids", plotOutput("solids_hist")),
+                        tabPanel("Chloramines", plotOutput("chloramines_hist")),
+                        tabPanel("Sulfate", plotOutput("sulfate_hist")),
+                        tabPanel("Conductivity", plotOutput("conductivity_hist")),
+                        tabPanel("Organic carbon", plotOutput("organic_carbon_hist")),
+                        tabPanel("Trihalomethanes", plotOutput("trihalomethanes_hist")),
+                        tabPanel("Turbidity", plotOutput("turbidity_hist"))
+                    )
+                )
+                ),
+                column(width=4, 
+                       h1("#TODO")),
+                column(width=4, 
+                       h1("#TODO"))
             ),
-            tabItem(tabName = "missing_values_tab",
-                    h1("Add options for handling missing values and summarising 
-                       the results"))
+            
+            fluidRow(
+                column(width=3, 
+                       h1("#TODO")),
+                column(width=6, 
+                       h1("#TODO")),
+                column(width=3, 
+                       h1("#TODO"))
+            )
+            #tabItem end
+            )
         )
     )
     
